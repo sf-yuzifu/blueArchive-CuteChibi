dcb678d3b2c1d59a4558a733b6b3021b1a8ecee0
小鱼yuzifu <yuzifu@yuzifu.top>
2023-10-21 23:34:30 +0800

    [add] add pwa

--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
 import './assets/index.css'
 import '@arco-design/web-vue/dist/arco.css'
 
-import {createApp} from 'vue'
+import { createApp } from 'vue'
+import { Modal } from '@arco-design/web-vue'
 import ArcoVue from '@arco-design/web-vue'
 import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 import App from './App.vue'
+import { registerSW } from 'virtual:pwa-register'
 
 const app = createApp(App)
 app.use(ArcoVue)
 app.use(ArcoVueIcon)
 
 app.mount('#app')
+
+if ('serviceWorker' in navigator) {
+  const updateSW = registerSW({
+    onNeedRefresh() {
+      Modal.confirm({
+        title: '注意',
+        content: '当前版本有更新，请问是否立即更新。',
+        okText: '确认',
+        onOk: () => {
+          updateSW(true)
+        },
+        onCancel: false,
+        hideCancel: false
+      })
+    }
+  })
+}
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import viteCompression from 'vite-plugin-compression'
 import viteImagemin from 'vite-plugin-imagemin'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -38,6 +39,49 @@ export default defineConfig({
           {
             name: 'removeEmptyAttrs',
             active: false
+          }
+        ]
+      }
+    }),
+    VitePWA({
+      mode: 'production',
+      base: '/',
+      registerType: 'prompt',
+      injectRegister: 'auto',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'ba-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
+      manifest: {
+        name: 'BlueArchive-ZhubiDatou',
+        short_name: 'BlueArchive-ZhubiDatou',
+        description: 'A project to select the best picture in Blue Archive.',
+        theme_color: '#128AFA',
+        start_url: '/',
+        id: 'ZhubiDatou',
+        icons: [
+          {
+            src: 'favicon.jpg',
+            sizes: '720x720',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'favicon.jpg',
+            sizes: '720x720'
           }
         ]
       }
