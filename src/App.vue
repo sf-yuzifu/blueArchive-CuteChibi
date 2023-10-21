3d5f884f98edbb5fb63776cccc2b54832174d862
小鱼yuzifu <yuzifu@yuzifu.top>
2023-10-21 19:14:52 +0800

    [add] add the core function

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
-import Header from "./components/theHeader.vue";
+import Header from './components/theHeader.vue'
+import Pic from './components/thePicture.vue'
 </script>
 
 <template>
   <Header></Header>
+  <div class="container">
+    <Pic></Pic>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/thePicture.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+
+const files = import.meta.glob('/public/pic/*.jpg')
+const zhubi = ref('')
+
+let toggle = false
+
+zhubi.value = Object.keys(files)[0].replace('/public', '')
+
+const sleep = (timeout) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, timeout)
+  })
+}
+
+const change_zhubi = async () => {
+  toggle = !toggle
+  if (toggle) {
+    for (;;) {
+      for (const path in files) {
+        await sleep(10)
+        zhubi.value = path.replace('/public', '')
+        if (!toggle) break
+      }
+      if (!toggle) break
+    }
+  }
+}
+</script>
+
+<template>
+  <img id="zhubi" :src="zhubi" alt="" @click="change_zhubi" />
+</template>
+
+<style scoped>
+#zhubi {
+  max-width: 400px;
+  max-height: 400px;
+  width: 90%;
+}
+</style>
