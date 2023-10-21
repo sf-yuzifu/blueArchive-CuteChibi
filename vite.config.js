34eb10a0d3cfca3560e3f64d60d4676d4bbc6756
小鱼yuzifu <yuzifu@yuzifu.top>
2023-10-22 00:10:03 +0800

    [add] restore base64

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    assetsInlineLimit: 0,
+    assetsInlineLimit: 1145141919810,
     minify: 'esbuild'
   },
   plugins: [
