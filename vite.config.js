7bf4f09fcdc62344be91609db2c99a477295d85b
小鱼yuzifu <yuzifu@yuzifu.top>
2023-10-21 23:56:23 +0800

    [remove] remove base64

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    assetsInlineLimit: 1145141919810,
+    assetsInlineLimit: 0,
     minify: 'esbuild'
   },
   plugins: [
